73ba952c9a69d22dcc4abbf9cd5872265666cc6b
Stanislavenko Yevgen <stanislavenkoyevgen@gmail.com>
2021-02-11 19:49:10 +0200

    practice 2

--- script.js
+++ script.js
@@ -1,75 +1,78 @@
-let numberOfFilms;
-
-
- function start() {
-   numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '');
-
-   while (numberOfFilms == '' || numberOfFilms == null ||  isNaN(numberOfFilms)) {
-      numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '');
-   }
- }
-
- start();
-
 const personalMovieDB = {
-   count: numberOfFilms,
+   count: 0,
    movies: {},
    actor: {},
    genres: [],
-   privat: false
+   privat: false,
+   start: function () {
+      personalMovieDB.count = prompt('Сколько фильмов уже посмотрели?', '');
+
+      while (personalMovieDB == '' || personalMovieDB == null || isNaN(personalMovieDB)) {
+         personalMovieDB.count = prompt('Сколько фильмов уже посмотрели?', '');
+      }
+   },
+   rememberMyFilm: function () {
+      for (let i = 0; i < 2; i++) {
+
+         const questionNumberOne = prompt('Один из последних просмотреных фильмов?', ''),
+            questionNumberTwo = prompt('На сколько оцените его', '');
+
+         if (questionNumberOne != null && questionNumberTwo != null && questionNumberOne != '' && questionNumberTwo != '' && questionNumberOne.length < 50) {
+            personalMovieDB.movies[questionNumberOne] = questionNumberTwo;
+            console.log('Done');
+         } else {
+            console.log('error');
+            i--;
+         }
+
+      }
+   },
+   detectPersonalLevel: function () {
+      if (personalMovieDB.count < 10) {
+         console.log("Просмотрено мало фильмов");
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+         console.log("Вы классический зритель");
+      } else if (personalMovieDB.count >= 30) {
+         console.log("Вы киноман");
+      } else {
+         console.log('Призошла ошибка');
+      }
+
+   },
+
+   showMyDB: function (hidden) {
+      if (!hidden) {
+         console.log(personalMovieDB);
+      }
+   },
+   toggleVisibleDB: function () {
+      if (personalMovieDB.privat) {
+         personalMovieDB.privat = false;
+      } else {
+         personalMovieDB.privat = true;
+      }
+   },
+
+   writeYourGenres: function () {
+      for (let i = 1; i <= 3; i++) {
+         let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+         if (genre == '' || genre == null) {
+            console.log('Вы не ввели некорретные данные или не ввели их');
+            i--;
+         } else {
+            personalMovieDB.genres[i - 1] = genre;
+         }
+
+      }
+
+
+      personalMovieDB.genres.forEach((item, i) => {
+         console.log(`Любимый жанр ${i+1}- это ${item}`);
+      });
+
+   }
+
+
 
 };
-
-
-function rememberMyFilm() {
-   for (let i = 0; i < 2; i++) {
-   
-      const questionNumberOne = prompt('Один из последних просмотреных фильмов?', ''),
-       questionNumberTwo = prompt('На сколько оцените его', '');
-      
-      if (questionNumberOne != null && questionNumberTwo != null && questionNumberOne != '' && questionNumberTwo !='' && questionNumberOne.length < 50) {
-         personalMovieDB.movies[questionNumberOne] = questionNumberTwo;
-          console.log('Done');
-      } else {
-          console.log ('error');
-          i--;
-      }
-      
-      }
-}
-
-rememberMyFilm();
-
-
-function detectPersonalLevel() {
-   if (personalMovieDB.count <  10) {
-      console.log ("Просмотрено мало фильмов");
-   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-      console.log("Вы классический зритель");
-   } else if (personalMovieDB.count >= 30) {
-      console.log("Вы киноман");
-   } else {
-      console.log('Призошла ошибка');
-   }
-   
-}
-
-detectPersonalLevel();
-
-
-function showMyDB (hidden) {
-   if (!hidden) {
-      console.log(personalMovieDB);
-   }
-}
-showMyDB(personalMovieDB.privat);
-
-
-function writeYourGenres () {
-   for (let i = 1; i <= 3; i++) {
-      let genres1 = prompt(`Ваш любимый жанр под номером ${i}`);
-      personalMovieDB.genres[i - 1] = genres1; 
-   }
-
-}
- writeYourGenres();
